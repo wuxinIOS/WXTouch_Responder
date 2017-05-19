2e926be37f757cd680508c40450aed8686f9fd29
wuxinIOS <wuxinIOS@163.com>
2017-05-19 10:08:26 +0800

    Initial Commit

---
+++ WXTouch_Responder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXTouch_Responder
+//
+//  Created by BlackEr Gray on 17/5/19.
+//  Copyright © 2017年 BlackEr Gray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WXTouch_Responder/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WXTouch_Responder
+//
+//  Created by BlackEr Gray on 17/5/19.
+//  Copyright © 2017年 BlackEr Gray. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
